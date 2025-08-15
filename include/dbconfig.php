df9c0e1a1b8bd0c61aea10d347caae073f4836b3
kayKhaingWin1 <helilin15@gmail.com>
2025-08-15 11:16:11 +0630

    update dbconfig by web.aeonfree

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -7,13 +7,13 @@ class Database {
     public static function connect()
     {
         if (!self::$connection) {
-            $hostname = getenv('DB_HOST');
-            $port     = getenv('DB_PORT');
-            $username = getenv('DB_USER');
-            $password = getenv('DB_PASS');
-            $dbname   = getenv('DB_NAME');
+            $hostname = 'sql101.iceiy.com'; 
+            $port     = '3306';             
+            $username = 'icei_39711552';   
+            $password = 'sally2002911';   
+            $dbname   = 'icei_39711552_food_order'; 
 
-            $dsn = "mysql:host=$hostname;port=$port;dbname=$dbname";
+            $dsn = "mysql:host=$hostname;port=$port;dbname=$dbname;charset=utf8mb4";
 
             try {
                 self::$connection = new PDO($dsn, $username, $password);
